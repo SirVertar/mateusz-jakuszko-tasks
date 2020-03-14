ffe2955eb6a36e65543e7de13a2d375ac52698c3
SirVertar <mateusz.jakuszko@gmail.com>
2020-03-14 10:55:56 +0100

    Task 19.2

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,4 +1,16 @@
 package com.crud.tasks.repository;
 
-public interface TaskRepository {
+import com.crud.tasks.domain.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+    @Override
+    List<Task> findAll();
+
+    Optional<Task> findById(Long id);
 }
