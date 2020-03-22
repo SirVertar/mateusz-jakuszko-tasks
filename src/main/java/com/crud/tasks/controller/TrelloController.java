2dae934eb5c944616b3ad5fe570759185b068985
SirVertar <mateusz.jakuszko@gmail.com>
2020-03-22 15:15:17 +0100

    Task 22.4

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -22,33 +22,10 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
+    public List<TrelloBoardDto> getTrelloBoards() {
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-//        final List<TrelloBoardDto> trelloBoardsOptional = trelloBoards.stream()
-//                .map(Optional::ofNullable)
-//                .map(e -> e.orElse(new TrelloBoardDto("empty", "empty", false)))
-//                .collect(Collectors.toList());
-
-//        trelloBoards.stream()
-//                .filter(e -> e.getId() != null)
-//                .filter(e -> e.getName() != null)
-//                .filter(e ->e.getName().contains("Kodilla"))
-//                .forEach(e -> System.out.println(e.getId() + " " + e.getName()));
-
-//        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
-
-        trelloBoards.forEach(trelloBoardDto -> {
-
-            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-
-            System.out.println("This board contains lists: ");
-
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-
-        });
-
+        return trelloBoards;
     }
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
     public CreatedTrelloCard createdTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
